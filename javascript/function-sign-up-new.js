6e914551495ecb3aaf0b824df60376e499d2c3f9
dularnikode <dularnikode@gmail.com>
2020-02-19 21:13:39 +0530

    corrected signin Functionality

--- javascript/function-sign-up-new.js
+++ javascript/function-sign-up-new.js
@@ -1,4 +1,8 @@
 "use strict";
+let flagFirstname=false;
+let flagLastname=false;
+let flagUsername=false;
+let flagPassword=false;
 
 function submitk(){
 
@@ -19,7 +23,7 @@ function submitk(){
     {
         alert("*Please fill required details.");
     }
-    else
+    else if(flagFirstname && flagLastname && flagPassword && flagUsername)
     {
         if(fem=="Female"){
             gender=fem;
@@ -43,22 +47,33 @@ function submitk(){
         let data=JSON.parse(localStorage.getItem(userName));
         console.log(data);
         alert("all sucessfull");
-
+    } 
+    else{
+        alert("*Please fill valid details");
     }
 }
+
 
 
 function validfName(){
     let firstName=document.getElementById("fname").value;
     let errField=document.getElementById("fnameError");
     let errorMessage="";
-    if(firstName==""){errorMessage="*Field can't be empty";}
-    else if(isNaN(firstName)!=true){errorMessage="*First Name can't be number";}
+    if(firstName==""){
+        errorMessage="*Field can't be empty";
+        flagFirstname=false;
+    }
+    else if(isNaN(firstName)!=true){
+        errorMessage="*First Name can't be number";
+        flagFirstname=false;
+    }
     else if((firstName.length)<2 || (firstName.length)>15){
         errorMessage="*Name can't be less than 2 and greather than 15 character";
+        flagFirstname=false;
     }
     else {
         errField.innerHTML="";
+        flagFirstname=true;
     }
     errField.innerHTML=errorMessage;
 }
@@ -67,13 +82,18 @@ function validlName(){
     let lastName=document.getElementById("lname").value;
     let errField=document.getElementById("lnameError");
     let errorMessage="";
-    if(lastName==""){errorMessage="*Field can't be empty";}
-    else if(isNaN(lastName)!=true){errorMessage="*Last Name can't be number";}
+    if(lastName==""){errorMessage="*Field can't be empty";flagLastname=false;}
+    else if(isNaN(lastName)!=true){
+        errorMessage="*Last Name can't be number";
+        flagLastname=false;
+    }
     else if((lastName.length)<2 || (lastName.length)>15){
         errorMessage="*Last Name can't be less than 2 and greather than 15 character";
+        flagLastname=false;
     }
     else {
         errField.innerHTML=errorMessage;
+        flagLastname=true;
     }
     errField.innerHTML=errorMessage;
 }
@@ -82,13 +102,21 @@ function validuName(){
     let userName=document.getElementById("username").value;
     let errField=document.getElementById("usernameError");
     let errorMessage="";
-    if(userName==""){errorMessage="*Field can't be empty";}
-    else if(isNaN(userName)!=true){errorMessage="*Username can't be number";}
+    if(userName==""){
+        errorMessage="*Field can't be empty";
+        flagUsername=false;
+    }
+    else if(isNaN(userName)!=true){
+        errorMessage="*Username can't be number";
+        flagUsername=false;
+    }
     else if((userName.length)<8 || (userName.length)>20){
         errorMessage="*Username can't be less than 8 and greather than 20 character";
+        flagUsername=false;
     }
     else {
         errField.innerHTML=errorMessage;
+        flagUsername=true;
     }
     errField.innerHTML=errorMessage;
 }
@@ -97,12 +125,30 @@ function validPassword(){
     let password=document.getElementById("password").value;
     let errField=document.getElementById("passwordError");
     let errorMessage="";
-    if(password==""){errorMessage="Field can't be empty";}
+    if(password==""){
+        errorMessage="Field can't be empty";
+        flagPassword=false;
+    }
     else if((password.length)<8 || (password.length)>15){
         errorMessage="Password can't be less than 8 and greather than 15 character";
+        flagPassword=false;
     }
     else {
         errField.innerHTML=errorMessage;
+        flagPassword=true;
     }
     errField.innerHTML=errorMessage;
 }
+
+// function validD
+
+// let firstName=document.getElementById("fname").value;
+// let lastName=document.getElementById("lname").value;
+// let userName=document.getElementById("username").value;
+// let password=document.getElementById("password").value;
+// let address=document.getElementById("address").value;
+// let profileImage=document.getElementById("profilephoto").value;
+
+// let gender;
+// let fem=document.getElementById("male").value;
+// let mal=document.getElementById("female").value;
